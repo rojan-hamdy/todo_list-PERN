7f5f63dbd9f7dbf043b13f7d95f15146d70590bf
rojan-hamdy <rojanhs2@gmail.com>
2026-09-10 21:51:22 +0200

    preparation for deployment

--- server/config/db.js
+++ server/config/db.js
@@ -1,13 +1,10 @@
-const {Pool} =require('pg');
-const dotenv =require('dotenv');
+const { Pool } = require('pg');
+const dotenv = require('dotenv');
 dotenv.config();
 
-const pool = new Pool({ 
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false
 });
 pool.on("connect", () => {
   console.log("Connected to the database");
